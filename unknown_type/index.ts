e44a64d406ea2ac17e5e818495295fb932fe22f5
Vinh Le <lethanhvinh95@gmail.com>
2022-01-06 08:13:53 +0200

    add assert cat function

--- unknown_type/index.ts
+++ unknown_type/index.ts
@@ -43,32 +43,57 @@ const isAnimal = (input: unknown): input is Animal => {
   && typeof input['age'] === 'number'
 }
 
-const isCat = (animal: unknown): animal is Cat => {
-  return isAnimal(animal) && animal['type'] === 'cat'
+const typeguards = {
+  isCat: (animal: unknown): animal is Cat => {
+    return isAnimal(animal) && animal['type'] === 'cat'
+  },
+  isDog: (animal: unknown): animal is Dog => {
+    return isAnimal(animal) && animal['type'] === 'dog'
+  }
 }
 
-const isDog = (animal: unknown): animal is Dog => {
-  return isAnimal(animal) && animal['type'] === 'dog'
-}
-
+/**
+ * Use type guards isCat and isDog to check the type for an unknown animal
+ * 
+ * @param animal 
+ * @returns 
+ */
 const addToList = (animal: unknown): Animal[] => {
-  if(isCat(animal)) {
+  if(typeguards.isCat(animal)) {
     return CATS.concat(animal)
-  } else if (isDog(animal)) {
+  } else if (typeguards.isDog(animal)) {
     return DOGS.concat(animal)
   }
 
   throw new Error(`Unknow animal: ${JSON.stringify(animal)}. Skip concatination`)
 }
 
-// Add a cat to list
-log(addToList({type: 'cat', name: 'baz', age: 3}))
-// Invalid cat
-addToList({type: 'cat'})
+// // Add a cat to list
+// log(addToList({type: 'cat', name: 'baz', age: 3}))
+// // Invalid cat
+// addToList({type: 'cat'})
 
-// Add a dog to list
-log(addToList({type: 'dog', name: 'baz', age: 3}))
-// Invalid dog
-addToList({type: 'dog', name: 12})
+// // Add a dog to list
+// log(addToList({type: 'dog', name: 'baz', age: 3}))
+// // Invalid dog
+// addToList({type: 'dog', name: 12})
 
-addToList({foo: 'bar'})
+// addToList({foo: 'bar'})
+
+// 
+// ------------------ Another version of addToList using assertion function --------------------
+//
+function assertCat(animal: unknown): asserts animal is Cat {
+  if(!isAnimal(animal) || animal['type'] !== 'cat') {
+    throw new Error(`${JSON.stringify(animal)} is not a cat`)
+  }
+}
+
+const addCatToList = (animal: unknown): Cat[] => {
+  assertCat(animal)
+
+  return CATS.concat(animal)
+}
+
+log(addCatToList({type: 'cat', name: 'baz', age: 3}))
+log(addCatToList({type: 'dog', name: 'baz', age: 3}))
